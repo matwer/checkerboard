606c1be769c6f5f3665f78430a02c2e19de9e313
Matthew Werner <matwer@outlook.com>
2021-05-07 21:33:00 -0700

    added sensei challenge

--- server.py
+++ server.py
@@ -17,6 +17,11 @@ def eight_by_four():
 def customBoard(rows, columns):
     return render_template("index3.html", rows = rows, columns = columns) 
 
+@app.route('/<int:rows>/<int:columns>/<color1>/<color2>')
+
+def customColorBoard(rows, columns, color1, color2):
+    return render_template("index4.html", rows = rows, columns = columns, color1 = color1, color2 = color2) 
+
 
 if __name__=="__main__":
     app.run(debug=True)
